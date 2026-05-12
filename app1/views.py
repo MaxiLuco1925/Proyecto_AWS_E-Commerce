da3810bd9ed660590ffc8ba7eb19b5ff1c4c8ce0
Ignacio <ignacio.leiva19@inacapmail.cl>
2026-05-12 17:16:56 -0400

    correcion de error en la vista

--- app1/views.py
+++ app1/views.py
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-from django.shortcuts import render
-from app1.models import Producto
-
-
-def listado_productos(request):
-    max_productos = 12
-    productos = Producto.objects.all()[:max_productos]
-    carousel_productos = productos[:8]
-    return render(request, 'lista_productos.html', {
-=======
 import boto3
 import time
 from django.shortcuts import render, redirect
@@ -72,35 +61,10 @@ def home(request):
     productos = Producto.objects.all()[:max_productos]
     carousel_productos = productos[:8]
     return render(request, 'home.html', {
->>>>>>> 7d890b7 (Avance final)
         'productos': productos,
         'carousel_productos': carousel_productos,
     })
 
-<<<<<<< HEAD
-
-def catalogo_tecnologia(request):
-    subcategorias = [
-        {'nombre': 'Computación', 'descripcion': 'Laptops, accesorios y componentes para trabajar y estudiar.'},
-        {'nombre': 'Audio', 'descripcion': 'Auriculares, bocinas y soluciones de sonido para el día a día.'},
-        {'nombre': 'Gaming', 'descripcion': 'Hardware y periféricos para gaming a otro nivel.'},
-        {'nombre': 'Gadgets', 'descripcion': 'Dispositivos inteligentes y chucherías tecnológicas.'},
-    ]
-    return render(request, 'catalogo_tecnologia.html', {'subcategorias': subcategorias})
-
-def vista_categorias(request, categoria_slug):
-    template_name = f'catalogo_{categoria_slug}.html'
-    return render(request, template_name, {
-        'categoria': categoria_slug.capitalize()
-    })
-
-def productos_por_subcategoria(request, subcategoria_nombre):
-    productos = Producto.objects.filter(categoria__iexact=subcategoria_nombre)
-    return render(request, 'productos_subcategoria.html', {
-        'productos': productos,
-        'subcategoria': subcategoria_nombre
-    })
-=======
 from django.contrib.auth.hashers import check_password # IMPORTANTE
 
 @csrf_protect
@@ -378,4 +342,3 @@ def checkout(request):
 
 def confirmacion_compra(request):
     return render(request, 'confirmacion_compra.html')
->>>>>>> 7d890b7 (Avance final)
