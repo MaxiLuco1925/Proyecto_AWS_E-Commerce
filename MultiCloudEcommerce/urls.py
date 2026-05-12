db1227e138845d7e898e5b784e0da6c91fd97c20
Ignacio <ignacio.leiva19@inacapmail.cl>
2026-05-12 17:35:04 -0400

    correcion de error en url

--- MultiCloudEcommerce/urls.py
+++ MultiCloudEcommerce/urls.py
@@ -1,18 +1,4 @@
 
-from django.contrib import admin
-from django.urls import path
-from app1.views import listado_productos, catalogo_tecnologia, vista_categorias, productos_por_subcategoria
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', listado_productos, name='home'),
-    path('tecnologia/', catalogo_tecnologia, name='catalogo_tecnologia'),
-    path('catalogo/<str:categoria_slug>/', vista_categorias, name='vista_categorias'),
-    path('productos/<str:subcategoria_nombre>/', productos_por_subcategoria, name='productos_subcategoria'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 from django.urls import path
 from app1.views import home, registro, iniciar_sesion, perfil, listado_pedidos, salir, carrito, agregar_carrito, checkout, confirmacion_compra
 from django.contrib import admin
